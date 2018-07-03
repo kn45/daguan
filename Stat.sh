0856cbb5dc1a13ec5de6ef69d8ab3cc2183cde2a
kn45 <381404825@qq.com>
2018-07-03 19:08:29 +0800

    update

--- Stat.sh
+++ Stat.sh
@@ -3,6 +3,9 @@
 data_raw=data_all/train_set.csv
 data_all=data_all/data_all.tsv
 
-cat $data_raw | awk -F',' '{print $4}' | uniq | sort | uniq > data_all/stat_clz
-cat $data_raw | awk -F',' '{print $2}' | tr ' ' '\n' | sort | uniq > data_all/stat_char
-cat $data_raw | awk -F',' '{print $3}' | tr ' ' '\n' | sort | uniq > data_all/stat_word
+cat $data_raw | awk -F',' '{print $4}' | uniq | sort -n | uniq > data_all/stat_clz
+cat $data_raw | awk -F',' '{print $2}' | tr ' ' '\n' | sort -n | uniq > data_all/stat_char
+cat $data_raw | awk -F',' '{print $3}' | tr ' ' '\n' | sort -n | uniq > data_all/stat_word
+
+cat $data_raw | awk -F',' '{print $2}' | awk -F' ' '{print NF}' | sort -n > data_all/stat_doc_len_by_char
+cat $data_raw | awk -F',' '{print $3}' | awk -F' ' '{print NF}' | sort -n > data_all/stat_doc_len_by_word
